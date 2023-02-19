54db8ce5534e7ff7d5f254ba62d1382dd1030360
AnjelicaB <116692684+AnjelicaB@users.noreply.github.com>
2023-02-19 16:54:59 -0500

    Update icpregistration.py

--- icpregistration.py
+++ icpregistration.py
@@ -5,16 +5,19 @@ import os
 import sys
 
 def draw_registration_result(source, target, transformation):
+    print(source)
     source_temp = copy.deepcopy(source)
     target_temp = copy.deepcopy(target)
     source_temp.paint_uniform_color([1, 0.706, 0])
     target_temp.paint_uniform_color([0, 0.651, 0.929])
     source_temp.transform(transformation)
-    o3d.visualization.draw_geometries([source_temp, target_temp],
-                                      zoom=0.4459,
-                                      front=[0.9288, -0.2951, -0.2242],
-                                      lookat=[1.6784, 2.0612, 1.4451],
-                                      up=[-0.3402, -0.9189, -0.1996])
+    # o3d.visualization.draw_geometries([source_temp, target_temp],
+    #                                   zoom=0.4459,
+    #                                   front=[0.9288, -0.2951, -0.2242],
+    #                                   lookat=[1.6784, 2.0612, 1.4451],
+    #                                   up=[-0.3402, -0.9189, -0.1996])
+    o3d.visualization.draw_geometries([source_temp, target_temp])
+
 
 
 # inputs
@@ -22,7 +25,7 @@ def draw_registration_result(source, target, transformation):
 source = o3d.io.read_point_cloud('out1.ply')
 target = o3d.io.read_point_cloud('out2.ply')
 
-threshold = 1
+threshold = 0.00001
 trans_init = np.asarray([[0.862, 0.011, -0.507, 0.5],
                          [-0.139, 0.967, -0.215, 0.7],
                          [0.487, 0.255, 0.835, -1.4], [0.0, 0.0, 0.0, 1.0]])
