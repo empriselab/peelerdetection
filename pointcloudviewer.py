1cc39e5775df6a486095878ddb824cfac4c8bb38
AnjelicaB <116692684+AnjelicaB@users.noreply.github.com>
2023-02-19 17:02:53 -0500

    Update pointcloudviewer.py

--- pointcloudviewer.py
+++ pointcloudviewer.py
@@ -301,6 +301,19 @@ while True:
         color_image = np.asanyarray(color_frame.get_data())
 
         depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha = 0.03), cv2.COLORMAP_JET)
+        
+        depth_colormap_dim = depth_colormap.shape
+        color_colormap_dim = color_image.shape
+        
+        if depth_colormap_dim != color_colormap_dim:
+            resized_color_image = cv2.resize(
+                color_image,
+                dsize=(depth_colormap_dim[1], depth_colormap_dim[0]),
+                interpolation=cv2.INTER_AREA,
+            )
+            images = np.hstack((resized_color_image, depth_colormap))
+        else:
+            images = np.hstack((color_image, depth_colormap))
 
         if state.color:
             mapped_frame, color_source = color_frame, color_image
