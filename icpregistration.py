1eedab05152dfcb71edd91ab4f6fe1f0bd087b4a
AnjelicaB <116692684+AnjelicaB@users.noreply.github.com>
2023-03-16 14:03:53 -0700

    Update icpregistration.py

--- icpregistration.py
+++ icpregistration.py
@@ -11,25 +11,56 @@ def draw_registration_result(source, target, transformation):
     source_temp.paint_uniform_color([1, 0.706, 0])
     target_temp.paint_uniform_color([0, 0.651, 0.929])
     source_temp.transform(transformation)
-    # o3d.visualization.draw_geometries([source_temp, target_temp],
-    #                                   zoom=0.4459,
-    #                                   front=[0.9288, -0.2951, -0.2242],
-    #                                   lookat=[1.6784, 2.0612, 1.4451],
-    #                                   up=[-0.3402, -0.9189, -0.1996])
+    o3d.visualization.draw_geometries([source_temp, target_temp],
+                                      zoom=0.4459,
+                                      front=[0.9288, -0.2951, -0.2242],
+                                      lookat=[1.6784, 2.0612, 1.4451],
+                                      up=[-0.3402, -0.9189, -0.1996])
     o3d.visualization.draw_geometries([source_temp, target_temp])
 
+def preprocess_point_cloud(pcd, voxel_size):
+    print(":: Downsample with a voxel size %.3f." % voxel_size)
+    pcd_down = pcd.voxel_down_sample(voxel_size)
 
+    radius_normal = voxel_size * 2
+    print(":: Estimate normal with search radius %.3f." % radius_normal)
+    pcd_down.estimate_normals(
+        o3d.geometry.KDTreeSearchParamHybrid(radius=radius_normal, max_nn=30))
+
+    radius_feature = voxel_size * 5
+    print(":: Compute FPFH feature with search radius %.3f." % radius_feature)
+    pcd_fpfh = o3d.pipelines.registration.compute_fpfh_feature(
+        pcd_down,
+        o3d.geometry.KDTreeSearchParamHybrid(radius=radius_feature, max_nn=100))
+    return pcd_down, pcd_fpfh
+
+def prepare_dataset(voxel_size):
+    print(":: Load two point clouds and disturb initial pose.")
+
+    demo_icp_pcds = o3d.data.DemoICPPointClouds()
+    source = o3d.io.read_point_cloud('out1.ply')
+    target = o3d.io.read_point_cloud('out2.ply')
+    trans_init = np.asarray([[0.0, 0.0, 1.0, 0.0], [1.0, 0.0, 0.0, 0.0],
+                             [0.0, 1.0, 0.0, 0.0], [0.0, 0.0, 0.0, 1.0]])
+    source.transform(trans_init)
+    draw_registration_result(source, target, np.identity(4))
+
+    source_down, source_fpfh = preprocess_point_cloud(source, voxel_size)
+    target_down, target_fpfh = preprocess_point_cloud(target, voxel_size)
+    return source, target, source_down, target_down, source_fpfh, target_fpfh
+
+voxel_size = 0.05  # means 5cm for this dataset
+source, target, source_down, target_down, source_fpfh, target_fpfh = prepare_dataset(
+    voxel_size)
 
 # inputs
 #demo_icp_pcds = o3d.data.DemoICPPointClouds()
-source = o3d.io.read_point_cloud('out1.ply')
-target = o3d.io.read_point_cloud('out2.ply')
 
 threshold = 0.00001
-trans_init = np.asarray([[0.862, 0.011, -0.507, 0.5],
-                         [-0.139, 0.967, -0.215, 0.7],
-                         [0.487, 0.255, 0.835, -1.4], [0.0, 0.0, 0.0, 1.0]])
-draw_registration_result(source, target, trans_init)
+#trans_init = np.asarray([[0.862, 0.011, -0.507, 0.5],
+#                         [-0.139, 0.967, -0.215, 0.7],
+#                         [0.487, 0.255, 0.835, -1.4], [0.0, 0.0, 0.0, 1.0]])
+#draw_registration_result(source, target, trans_init)
 
 print("Apply point-to-point ICP")
 reg_p2p = o3d.pipelines.registration.registration_icp(
